9962aeae0c34e5b4aa726c39a47da8469c8b8f18
RogueAlmond70 <mra.king2@gmail.com>
2022-11-08 12:36:17 +0000

    Created a setup sql script to get postGIS up and running. This allowed us to then create the geospatially enabled table, and then run sql to populate the table.

---
+++ Spotlas/Database/Create_Table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "MY_TABLE" (
+                            id uuid,
+                            name varchar,
+                            website varchar,
+                            coordinates geography,
+                            description varchar,
+                            rating float8
+);
